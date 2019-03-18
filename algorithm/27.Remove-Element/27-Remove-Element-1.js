3d79d9fd7bc8ebe6b6ae17c346e9dda84a73b404
zhuyue <18340826605@163.com>
2019-03-18 22:56:08 +0800

    add LeetCode.27

---
+++ algorithm/27.Remove-Element/27-Remove-Element-1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let len = nums.length;
+    for (let i = 0; i < len; i++) {
+        if (nums[i] === val) {
+            nums[i] = nums[len - 1];
+            len--;
+            i--;
+        }
+    }
+    return len;
+};
