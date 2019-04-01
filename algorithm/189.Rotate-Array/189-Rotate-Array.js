753cbf6f1a119a1515f68f8f079a9d8d9aca4760
zhuyue <18340826605@163.com>
2019-04-01 21:50:33 +0800

    add Leetcode.189

---
+++ algorithm/189.Rotate-Array/189-Rotate-Array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    let newNums = nums.splice(nums.length - k, k);
+    nums.unshift(...newNums);
+    return nums;
+};
